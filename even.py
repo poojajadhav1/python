fe0016a03bca5d3cc6ceeaea764b9bcea025db9d
poojajadhav1 <70567857+poojajadhav1@users.noreply.github.com>
2020-09-01 13:13:45 +0530

    Add files via upload

---
+++ even.py
@@ -0,0 +1,15 @@
+"""for i in range(0,20):
+    if i%2==0:
+        print("even no:",i)
+i=0
+while i<20:
+    print(i)
+    i+=1
+
+x=[i%2 !=0  for i in range(0,20)]
+print(x)"""
+
+n=int(input())
+for i in range(1,11):
+    print(n*i)
+    
